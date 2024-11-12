b249f2874bccf3cd745b9edf5d7d7e2f9fe2103d
Vinod Akshat <pumkininriver@gmail.com>
2024-11-12 22:45:06 +0530

    std::process::exit

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,24 @@ use std::io::{self, Write};
 
 fn main() {
     let stdin = io::stdin();
-    let mut input = String::new();
+    
+    loop {
+        prompt();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        handle_input(&input);
+    }
+}
+fn handle_input(input: &str) {
+    let mut parts = input.split(' ');
+    match parts.next().unwrap() {
+        "exit" => std::process::exit(parts.next().and_then(|x| x.parse().ok()).unwrap_or(0)),
+        "echo" => println!("{}", parts.collect::<Vec<&str>>().join(" ")),
+        _ => println!("{input}: command not found"),
+    }
+}
+fn prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
-    
-    while stdin.read_line(&mut input).is_ok() {
-        let trimmed = input.trim();
-        
-        if trimmed == "exit" {
-            break;
-        }
-        
-        if !trimmed.is_empty() {
-            println!("{}: command not found", trimmed);
-        }
-        
-        input.clear();
-        print!("$ ");
-        io::stdout().flush().unwrap();
-    }
 }
